d2b5d3870d38095b1bbea36d74a7d267aabfcfa4
Seowoopark55 <dls18222@naver.com>
2026-04-23 17:57:09 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -713,7 +713,7 @@ export default function App() {
                         fontWeight: "600"
                       }}
                     >
-                      안내: {storeNotice}
+                      {storeNotice}
                     </p>
                   )}
                 </div>
