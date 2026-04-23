82403b445dd023435a4155397827c0400fbe27dc
Seowoopark55 <dls18222@naver.com>
2026-04-23 15:31:35 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -523,7 +523,7 @@ export default function App() {
                 <div
                   style={{
   textAlign: "center",
-  paddingLeft: "36px",
+  paddingLeft: "60px",
   paddingRight: "0px"
 }}
                 >
